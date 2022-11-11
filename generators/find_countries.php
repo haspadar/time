b1c81ac59cb90c377d371af37e702cd02e64c58f
Kanstantsin <haspadar@gmail.com>
2022-11-11 13:29:58 +0300

    Added find_countries script

---
+++ generators/find_countries.php
@@ -0,0 +1,20 @@
+<?php
+require_once '../config.php';
+
+$countries = DB::queryFirstColumn('SELECT DISTINCT country_name_en FROM cities AS c WHERE country_name_en NOT IN(SELECT url FROM urls) AND REPLACE(country_name_en, " ", "_") NOT IN(SELECT url FROM urls) AND country_name_en <>""');
+foreach ($countries as $country) {
+    $url = strtr($country, [' ' => '_']);
+//    $found = DB::queryFirstRow('SELECT * FROM urls WHERE url=%s', $url);
+    $capital = DB::queryFirstRow('SELECT * FROM urls WHERE country=%s AND is_country_capital=1', $url);
+    if (!$capital) {
+        var_dump($country);
+    }
+//    DB::insert('urls', [
+//        'url' => $url,
+//        'country' => $country,
+//        'title' => $country,
+//        'coordinates'
+//        'timezone'
+//    ])
+}
+
